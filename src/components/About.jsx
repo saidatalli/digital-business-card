4991b923a616eebddf721e957a3fab1f32ca4e81
saidatalli <rabiusaidat007@gmail.com>
2022-09-01 16:45:32 +0100

    project setup

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+export default function About () {
+    return (
+        <div>
+            <h2>About Me</h2>
+            <p>Hi, I'm Saidat Alli. I'm a full-stack Software Engineer based in Newcastle upon Tyne, UK. I specialize in building and improving the online presence for my clients. My goal is to create a modern, responsive and user-friendly website and web app for you.
+            <br></br>
+            I also build business websites with SEO in mind, so you can rank higher on Google search results. When I am not coding, I do loads of cardio because I have a sweet tooth. I also enjoy trying new recipes.</p>
+        </div>
+    )
+}
---
+++ src/components/Interests.jsx
@@ -0,0 +1,8 @@
+export default function Interests () {
+    return (
+        <div>
+            <h2>Interests</h2>
+            <p>Skincare Lover. Scientist. Food expert. Korean Series Lover.</p>
+        </div>
+    )
+}

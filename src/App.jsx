42a9778d8d7f5eff5cbfb39460bd8fa92c68ed59
saidatalli <rabiusaidat007@gmail.com>
2022-09-01 19:39:30 +0100

    update component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Header from "./components/Header"
+import About from "./components/About"
+import Interests from "./components/Interests"
+import Footer from "./components/Footer"
 
 function App() {
 
   return (
-    <div className="App">
-      
+    <div className="App container">
+      <Header />
+      <About />
+      <Interests />
+      <Footer />
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
+import { FaTwitter, FaGithub, FaAngellist, FaHome} from 'react-icons/fa';
+
+
 export default function Footer () {
     return (
         <div className="footer">
-            
+            <FaTwitter className='footer-icons'/>
+            <FaGithub className='footer-icons' /> 
+            <FaAngellist className='footer-icons' />
+            <FaHome className='footer-icons' />
         </div>
     )
 }
